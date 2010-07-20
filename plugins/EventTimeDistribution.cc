5ee9c33ba6a6f091fc5eebc554f8cd9f56f239fa
cmsbuild <>
2010-07-20 19:19:08 +0000

    This commit was manufactured by cvs2git to create tag 'DPGAnalysis- SiStripTools-CMSSW_3_9_0_pre1'.
    Sprout from master 2010-03-11 10:14:30 UTC Andrea Venturi <sha1-71af2bda94b31c8bb1d8f97d0062b66e7418ae83@cern.ch> 'LargeEvents templated for pixel clusters. BX histogram added to EventTimeDistribution'
    Cherrypick from master 2010-07-20 19:19:07 UTC William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch> 'temporarily back out unapproved code and add missing includes':
        plugins/APVCyclePhaseMonitor.cc
        plugins/EventTimeDistribution.cc
        plugins/SiStripQualityHistory.cc
    Delete:
        python/testrepro/ConfigFile_GR09_dqmfilter_cfg.py
        python/testrepro/ConfigFile_GR09_dqmfilter_plustracking_cfg.py
        python/testrepro/ConfigFile_GR09_multfilter_cfg.py
        python/testrepro/ConfigFile_GR09_nofilter_cfg.py
        python/testrepro/ConfigFile_GR09_nofilter_plustracking_cfg.py
        python/testrepro/ConfigFile_GR09_tightdqmfilter_cfg.py
        python/testrepro/ConfigFile_asymmetry_cfg.py
        python/testrepro/ConfigFile_cfg.py
        python/testrepro/ConfigFile_dqmfilter_cfg.py
        python/testrepro/ConfigFile_multfilter_cfg.py
        python/testrepro/ConfigFile_nofilter_cfg.py
        python/testrepro/ConfigFile_tightdqmfilter_cfg.py
        python/testrepro/MessageLogger_cff.py
        python/testrepro/calzerobiasclusters_cff.py
        python/testrepro/clonedsistripquality_cff.py
        python/testrepro/cs_maps_cff.py
        python/testrepro/dqm_cff.py
        python/testrepro/dqm_withclonedclustermonitor_cff.py
        python/testrepro/globaltag_sistripquality_cff.py
        python/testrepro/inputsource_cff.py
        python/testrepro/noisyevent_filters_2008_cff.py
        python/testrepro/noisyevent_filters_2009_cff.py
        python/testrepro/noisyevent_filters_2009_withdefaultphase_cff.py
        python/testrepro/noisyevent_filters_withmultiplicity_cff.py
        python/testrepro/rawtodigi_cff.py
        python/testrepro/sistripqualitystatistics_cff.py
        python/testrepro/tecbadmodules_cff.py

--- plugins/EventTimeDistribution.cc
+++ plugins/EventTimeDistribution.cc
@@ -31,6 +31,7 @@
 
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/interface/Run.h"
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
